c48a3fa9f52e6f0813614a53771fca70af8fde2b
Dennis Neely <dennis.neely22@gmail.com>
2020-02-24 14:06:08 +0100

    added name & phone fields to registration & user edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :collections, dependent: :destroy
+
+  validates :first_name, :last_name, presence: true
+  validates :email, uniqueness: true, presence: true
+
+  # has_one_attached :avatar
 end
