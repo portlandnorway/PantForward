627d7ae528f9e47f2dfcaed6a5e24d3053eed5a0
Nikolai Toverud <nikolai.toverud@gmail.com>
2020-02-24 14:11:25 +0100

    Navbar created, and rendering the 'no navbar' view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      "no_navbar_layout"
+    else
+      "application"
+    end
+  end
 end
