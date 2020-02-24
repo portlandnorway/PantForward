575037dc458e476408dd6cf5e5d5338ab75b0413
Nikolai Toverud <nikolai.toverud@gmail.com>
2020-02-24 15:20:21 +0100

    Added some simple rules for the design headers and paragrapths. Still need to do some testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,12 +10,13 @@ class ApplicationController < ActionController::Base
     else
       "application"
     end
-    
+  end
+
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number, :avatar])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone_number, :avatar])  
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone_number, :avatar])
   end
 end
