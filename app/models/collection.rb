477403e477371068117949d5e316ccbd10fe966e
Dennis Neely <dennis.neely22@gmail.com>
2020-02-25 17:13:18 +0100

    fuck it

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,10 +1,9 @@
 class Collection < ApplicationRecord
+  belongs_to :user
+  has_many :bookings, dependent: :destroy
   geocoded_by :address
   validates :address, :bottles, presence: true
   after_validation :geocode, if: :will_save_change_to_address?
 
   enum status: [:available, :booked, :picked_up, :confirmed]
-
-  # belongs_to :donator, class_name: "User", foreign_key: :donator_id
-  # belongs_to :collector, class_name: "User", foreign_key: :collector_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :bookings, dependent: :destroy
+  has_many :collections, dependent: :destroy
+
   validates :first_name, :last_name, presence: true
   validates :email, uniqueness: true, presence: true
 
