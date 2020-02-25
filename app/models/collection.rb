d6095cb45e8f4d5b1a4c3d17c16f436460b8fecc
Dennis Neely <dennis.neely22@gmail.com>
2020-02-25 12:01:45 +0100

    added donator and collector to user

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -3,4 +3,7 @@ class Collection < ApplicationRecord
   validates :address, :bottles, presence: true
 
   enum status: [:available, :booked, :picked_up, :confirmed]
+
+  belongs_to :donator, class_name: "User", foreign_key: :donator_id
+  belongs_to :collector, class_name: "User", foreign_key: :collector_id
 end
