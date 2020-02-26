5ab56c4a6d63b24353d1a8460f5fcf538e80386d
Dennis Neely <dennis.neely22@gmail.com>
2020-02-26 15:26:54 +0100

    start of algolia

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,9 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 
 
 import { initMapbox }       from '../plugins/init_mapbox';
-import { modalOnBtnClick } from '../plugins/init_modal';
+import { modalOnBtnClick }  from '../plugins/init_modal';
+import { initAutocomplete } from '..plugins/init_autocomplete'
 
 initMapbox();
 modalOnBtnClick();
+initAutocomplete();
