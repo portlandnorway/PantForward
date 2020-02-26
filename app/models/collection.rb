1228617635a4d2e46e77536edd5538c023ef83f3
Dennis Neely <dennis.neely22@gmail.com>
2020-02-26 12:24:06 +0100

    updated model, database, seeds with Neighborhoods. Added filtering by neighborhood

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,8 +1,14 @@
 class Collection < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
-  geocoded_by :address
+
   validates :address, :bottles, presence: true
+
+  NEIGHBORHOODS = ['Bjerke', 'Bygdøy-Frogner', 'Bøler', 'Ekeberg-Bekkelaget', 'Furuset', 'Gamle Oslo', 'Grefsen - Kjelsås', 'Grorud', 'Grūnerløkka - Sofienberg', 'Hellerud','Helsfyr - Sinsen', 'Lambertseter', 'Manglerud', 'Marka', 'Nordstrand', 'Romsås', 'Røa', 'Sagene - Torshov', 'Sentrum', 'Sogn', 'Stovner', 'St. Hanshaugen - Ullevål', 'Søndre Nordstrand', 'Ullern', 'Uranienborg - Majorstuen', 'Vinderen', 'Østenjø']
+
+  validates :neighborhood, presence: true, inclusion: NEIGHBORHOODS
+
+  geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
   enum status: [:available, :booked, :picked_up, :confirmed]
